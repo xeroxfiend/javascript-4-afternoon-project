9e0ea0ea7d10da6a94172eda0a2f0bb3d1cdd6e1
Brian Lowe <mynameisboy@gmail.com>
2019-08-23 15:20:01 -0600

    almost finished with classes

--- classes.js
+++ classes.js
@@ -29,7 +29,22 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+    constructor(first_name, last_name, email, age) {
+        this.first_name = first_name
+        this.last_name = last_name
+        this.email = email
+        this.age = age
+    }
+
+    makeWidget() {
+        return `${this.first_name} ${this.last_name} Widget`
+    }
+}
+
+// let newEmployee = new Employee('Brian', 'Lowe')
+
+// console.log(newEmployee)
 
 
 ////////// PROBLEM 2 //////////
@@ -47,8 +62,20 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee {
+    constructor(first_name, last_name, email, age, reports = []) {
+        super(first_name, last_name, email, age)
+        this.reports = reports
+    }
 
+    hire(Employee) {
+        this.reports.push(Employee)
+    }
+
+    fire(index) {
+        this.reports.splice(index, 1)
+    }
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -71,8 +98,27 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager extends Manager {
+    constructor(first_name, last_name, email, age, reports, title = 'Not a manager', bonus = 0) {
+        super(first_name, last_name, email, age, reports)
+        this.title = title
+        this.bonus = bonus
+    }
 
+    managerTitle(ProgressiveManager) {
+        if (this.super.reports === 0) this.title = 'Not a manager'
+        else if (this.reports.length >= 1 && this.reports.length <= 3) this.title = 'Barely manager'
+        else if (this.reports.length >= 4 && this.reports.length <= 10) this.title = 'Mostly manager'
+        else if (this.reports.length >= 11 && this.reports.length <= 50) this.title = 'Manager'
+        else if (this.reports.length >= 51 && this.reports.length <= 100) this.title = 'Manager Plus'
+        else if (this.reports.length >= 101) this.title = 'Bestest manager'
+    }
+
+    fire() {
+        this.bonus += 100
+    }
+
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -99,5 +145,3 @@
 */
 
 //Code Here
-
-
